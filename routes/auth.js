e127e20dd8cdfaab557b005de53fba93b0f231c7
yashrajpahwa <59451283+yashrajpahwa@users.noreply.github.com>
2021-01-18 20:09:31 +0530

    Added rate limiting to authentication

--- routes/auth.js
+++ routes/auth.js
@@ -8,12 +8,23 @@ const {
 	resetPassword,
 } = require('../controllers/auth');
 const router = express.Router();
+const rateLimit = require('express-rate-limit');
 
-router.route('/initiateuser').post(sendMailToInitiateUser);
-router.route('/register/:registerToken').post(register);
-router.route('/forgotpassword').post(forgotPassword);
-router.route('/resetpassword/:resetpasswordtoken').put(resetPassword);
-router.route('/login').post(login);
+const loginLimiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 60, // limit each IP to 50 requests per windowMs
+});
+
+const limit = rateLimit({
+	windowMs: 12 * 60 * 1000, // 15 minutes
+	max: 80, // limit each IP to 50 requests per windowMs
+});
+
+router.route('/initiateuser').post(sendMailToInitiateUser, limit);
+router.route('/register/:registerToken').post(register, limit);
+router.route('/forgotpassword').post(forgotPassword, limit);
+router.route('/resetpassword/:resetpasswordtoken').put(resetPassword, limit);
+router.route('/login').post(login, loginLimiter);
 
 // Dev Routes
 if (process.env.NODE_ENV === 'development') {
