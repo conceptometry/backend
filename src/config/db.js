33fa37fce2b671549a47c0c6517817a8d0682362
Yashraj Pahwa <59451283+yashrajpahwa@users.noreply.github.com>
2021-02-24 06:31:10 +0530

    Upgraded mongoose version to 5.11.15

--- src/config/db.js
+++ src/config/db.js
@@ -2,15 +2,15 @@ const mongoose = require('mongoose');
 
 // Connect to database (mongo db)
 const dbConnect = async () => {
-	const DB_URI = process.env.MONGODB_URI;
-	const conn = await mongoose.connect(DB_URI, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-		useCreateIndex: true,
-		useFindAndModify: false,
-	});
+  const DB_URI = process.env.MONGODB_URI;
+  const conn = await mongoose.connect(DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  });
 
-	console.log(`MongoDB Connected: ${conn.connection.host}`.yellow.bold);
+  console.log(`MongoDB Connected: ${conn.connection.host}`.yellow.bold);
 };
 
 module.exports = dbConnect;
